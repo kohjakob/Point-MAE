e6e1eb53940406711ced350bc397eba4c3d308ac
Jakob Kohlhas <kohjakob@gmail.com>
2025-04-01 15:14:12 +0200

    Changed imports to work when executed together with other externals. Hacky solution, maybe clean this up some time in the future.

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-from .build import build_model_from_cfg
-import models.Point_MAE
+from external.PointMAE.models.build import build_model_from_cfg
+import external.PointMAE.models.Point_MAE
--- tools/builder.py
+++ tools/builder.py
@@ -4,8 +4,8 @@ import torch
 # optimizer
 import torch.optim as optim
 # dataloader
-from datasets import build_dataset_from_cfg
-from models import build_model_from_cfg
+from external.PointMAE.datasets import build_dataset_from_cfg
+from external.PointMAE.models import build_model_from_cfg
 # utils
 from utils.logger import *
 from utils.misc import *
